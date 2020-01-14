bfbc33e300ffaca3df60de9931d1bb8c1e09cf9b
jet_martin <jet_martin@hotmail.com>
2020-01-14 08:05:49 +0100

    Enhance using external config file

--- bash/goto/goto.sh
+++ bash/goto/goto.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##
 # GOTO Aliases
@@ -7,27 +7,86 @@
 # alias goto='. /pathToFile/goto.sh'
 #
 
-declare -A PATH_ALIAS
+##
+# Function fo fix iOS compatibility (Bash 3)
+#
+real_path () {
+  OIFS=$IFS
+  IFS='/'
+  for I in $1
+  do
+    # Resolve relative path punctuation.
+    if [ "$I" = "." ] || [ -z "$I" ]
+      then continue
+    elif [ "$I" = ".." ]
+      then FOO="${FOO%%/${FOO##*/}}"
+           continue
+      else FOO="${FOO}/${I}"
+    fi
+
+    ## Resolve symbolic links
+    if [ -h "$FOO" ]
+    then
+    IFS=$OIFS
+    set `ls -l "$FOO"`
+    while shift ;
+    do
+      if [ "$1" = "->" ]
+        then FOO=$2
+             shift $#
+             break
+      fi
+    done
+    IFS='/'
+    fi
+  done
+  IFS=$OIFS
+  echo "$FOO"
+}
 
 ##
-# Add here some path using the structure bellow :
-# PATH_ALIAS[arrayKey]=Path
+# Get Full path of the current script
 #
-
-# System aliases
-PATH_ALIAS[apache]=/etc/apache2
-# Static
-PATH_ALIAS[static]=/var/www/static
-
+# THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
+THIS=`real_path ${BASH_SOURCE[0]} 2>/dev/null||echo $0`
+DIR=`dirname "${THIS}"`
 
 ##
-# Execute CD
+# Manage bash release to check if parameter exist.
 #
+if [ ${BASH_VERSION::1} -gt 3 ]; then
+  declare -A PATH_ALIAS
+  . "$DIR/path_aliases.sh"
 
-# if [ -n ${PATH_ALIAS[$1]} ]
-if test "${PATH_ALIAS["$1"]+isset}"
-then
-  cd ${PATH_ALIAS[$1]}
+  if test "${PATH_ALIAS["$1"]+isset}"
+  then
+    PATH=${PATH_ALIAS[$1]}
+  else
+    echo "\"$1\" is not a defined alias.";
+  fi
 else
-  echo "\"$1\" is not a defined alias.";
+  sed -e 's/PATH_ALIAS\[/declare PATH_ALIAS_/g' -e 's/]=/=/g' $DIR/path_aliases.sh > $DIR/~path_aliases.sh
+  . "$DIR/~path_aliases.sh"
+
+  PARAM="PATH_ALIAS_${1}"
+  # echo "${!PARAM}"
+  # if test -z "${PARAM}"
+  if test "${!PARAM+isset}"
+  then
+    PATH=${!PARAM}
+  else
+    echo "\"$1\" is not a defined alias.";
+  fi
+fi
+
+##
+# Move to requested folder
+#
+if [ -d $PATH ]; then
+  # command cd $PATH
+  # echo "cd $PATH"
+  cd $PATH
+  # exec bash
+else
+  echo "The folder $PATH do not exist."
 fi
---
+++ bash/goto/path_aliases.sh
@@ -0,0 +1,9 @@
+##
+# Add here some path using the structure bellow :
+# PATH_ALIAS[arrayKey]=Path
+#
+
+# System aliases
+PATH_ALIAS[apache]=/etc/apache2
+# Static
+PATH_ALIAS[static]=/var/www/static
